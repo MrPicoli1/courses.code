0282eee72db4a255617907623aaa3f2769c52621
Guilherme Picoli <83010918+MrPicoli1@users.noreply.github.com>
2024-02-16 14:55:57 -0300

    Division of Nlogonia

---
+++ .Net/Code Challenges/CodeChallenges/DivisionOfNlogonia.cs
@@ -0,0 +1,56 @@
+﻿namespace CodeChallenges
+{
+    public class DivisionOfNlogonia
+    {
+
+        public void Location()
+        {
+
+            while (true)
+            {
+                try
+                {
+
+                    int vezes = Convert.ToInt32(Console.ReadLine());
+
+                    if (vezes == 0) { break; }
+
+                    string[] batata = Console.ReadLine().Split(' ');
+
+                    int a = Convert.ToInt32(batata[0]);
+                    int b = Convert.ToInt32(batata[1]);
+
+                    for (int i = 0; i < vezes; i++)
+                    {
+                        string[] batata2 = Console.ReadLine().Split(' ');
+                        int x = Convert.ToInt32(batata2[0]);
+                        int y = Convert.ToInt32(batata2[1]);
+                        if (x == a || y == b)
+                        {
+                            Console.WriteLine("divisa");
+                        }
+                        if (x < a && y < b)
+                        {
+                            Console.WriteLine("SO");
+                        }
+                        if (x > a && y < b)
+                        {
+                            Console.WriteLine("SE");
+                        }
+                        if (x < a && y > b)
+                        {
+                            Console.WriteLine("NO");
+                        }
+                        if (x > a && y > b)
+                        {
+                            Console.WriteLine("NE");
+                        }
+
+                    }
+                }
+                catch { }
+
+            }
+        }
+    }
+}
---
+++ .Net/Code Challenges/CodeChallenges/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            try
+            {
+
+                int vezes = Convert.ToInt32(Console.ReadLine());
+
+                if (vezes == 0) { break; }
+
+                string[] batata = Console.ReadLine().Split(' ');
+
+                int a = Convert.ToInt32(batata[0]);
+                int b = Convert.ToInt32(batata[1]);
+
+                for (int i = 0; i < vezes; i++)
+                {
+                    string[] batata2 = Console.ReadLine().Split(' ');
+                    int x = Convert.ToInt32(batata2[0]);
+                    int y = Convert.ToInt32(batata2[1]);
+                    if (x == a || y == b)
+                    {
+                        Console.WriteLine("divisa");
+                    }
+                    if (x < a && y < b)
+                    {
+                        Console.WriteLine("SO");
+                    }
+                    if (x > a && y < b)
+                    {
+                        Console.WriteLine("SE");
+                    }
+                    if (x < a && y > b)
+                    {
+                        Console.WriteLine("NO");
+                    }
+                    if (x > a && y > b)
+                    {
+                        Console.WriteLine("NE");
+                    }
+
+                }
+            }
+            catch { }
+
+        }
+    }
+}
